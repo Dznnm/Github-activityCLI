ad2fe0a8ac89cd0c8f9322b89d399492c7f4b8a5
Dznnm <danun.speed@gmail.com>
2026-06-17 19:56:55 +0700

    initial commit, req url, print activity

---
+++ github-activity.py
@@ -0,0 +1,26 @@
+import sys
+import json
+import urllib.request
+import urllib.error
+
+if sys.argv[0] == "ghactivityCLI.py":
+    username = sys.argv[1]
+    url = f"https://api.github.com/users/{username}/events/public"
+    urllib.request.urlopen(url)
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    events = json.loads(data)
+    for event in events:
+        repo_name = event["repo"]["name"]
+        if event["type"] == "PushEvent":
+            print(f"- Pushed to {repo_name}")
+        elif event["type"] == "CreateEvent":
+            print(f"- Created {repo_name}")
+        elif event["type"] == "DeleteEvent":
+            print(f"- Deleted {repo_name}")
+        elif event["type"] == "ForkEvent":
+            print(f"- Forked {repo_name}")
+        elif event["type"] == "MemberEvent":
+            print(f"- Added a member to {repo_name}")
+        else:
+            print(f"- {event['type']} in {repo_name}")
